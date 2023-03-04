5543c1b6724709d465db7b8481cef00b77d5edd1
권진호(Jinho Kwon)/Rider <jinho.kwon@wesang.com>
2023-03-04 23:16:40 +0900

    add section 9

---
+++ section_9/README.md
@@ -0,0 +1,50 @@
+# 섹션 9. 멀티 노드 카프카 클러스터
+
+
+## 멀티 노드 카프카 클러스터 개요 및 분산 시스템 장단점
+
+
+## 3개의 멀티 브로커로 카프카 구성하기
+
+
+## <중요> 멀티 브로커용 Zookeeper 설정에 따른 변경 공지
+
+
+## 멀티 브로커 카프카에서 여러 개의 파티션을 가지는 다중 복제 토픽 만들기
+
+
+## 카프카 Replication(복제)와 리더(Leader)/팔로워(Follower) 이해
+
+
+## 카프카 Replication(복제)와 리더(Leader)/팔로워(Follower) 실습
+
+
+## 멀티 브로커 환경에서 Producer의 bootstrap.servers 설정 이해
+
+
+## 주키퍼(Zookeeper)와 컨트롤러(Controller) 브로커의 이해
+
+
+## zookeeper_shell 명령어를 이용하여 주키퍼와 카프카 기동 메커니즘 이해 및 실습
+
+
+## 컨트롤러의 리더 선출(Leader Election) 프로세스 이해
+
+
+## 컨트롤러의 리더 선출(Leader Election) 프로세스 실습
+
+
+## 컨트롤러의 리더 선출(Leader Election) 프로세스 실습 후 공지
+
+
+## ISR(In-Sync_Replicas)의 이해
+
+
+## min.insync.replicas 설정에 따른 Producer 전송 이해
+
+
+## Preferred Leader Election 이해 및 실습
+
+
+## Unclean Leader Election 이해 및 실습
+
---
+++ section_9/simple_producer_sync.py
@@ -0,0 +1,47 @@
+import sys
+
+from confluent_kafka import Producer
+from section_2.settings import settings
+
+if __name__ == '__main__':
+    broker = settings.broker_url
+    topic = settings.simple_topic
+    print(f'Usage: <bootstrap-brokers> : {broker} <topic> {topic}')
+
+    # Producer configuration
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    conf = {'bootstrap.servers': broker}
+
+    # Create Producer instance
+    p = Producer(**conf)
+
+
+    # Optional per-message delivery callback (triggered by poll() or flush())
+    # when a message has been successfully delivered or permanently
+    # failed delivery (after retries).
+    def delivery_callback(err, msg):
+        if err:
+            sys.stderr.write('%% Message failed delivery: %s\n' % err)
+        else:
+            sys.stderr.write('%% Message delivered to %s [%d] @ %d\n' %
+                             (msg.topic(), msg.partition(), msg.offset()))
+
+
+    # Read lines from stdin, produce each line to Kafka
+    for line in sys.stdin:
+        try:
+            # Produce line (without newline)
+            p.produce(topic, line.rstrip(), callback=delivery_callback)
+            p.poll(0)  # 이럼 되나? 확인해봐야 할₩
+        except BufferError:
+            sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
+                             len(p))
+
+        # Serve delivery callback queue.
+        # NOTE: Since produce() is an asynchronous API this poll() call
+        #       will most likely not serve the delivery callback for the
+        #       last produce()d message.
+
+    # Wait until all messages have been delivered
+    sys.stderr.write('%% Waiting for %d deliveries\n' % len(p))
+    p.flush()
